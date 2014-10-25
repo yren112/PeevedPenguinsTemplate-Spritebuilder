1364f2b6666cb715f38e4ec06cb2da1211cc8d77
Cheng Ren <Crenberry@Xins-MacBook-Air.local>
2014-10-25 17:02:41 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Cheng Ren on 10/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Cheng Ren on 10/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
