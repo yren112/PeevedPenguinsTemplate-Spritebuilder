29ea07fe85d985ef4e07b420cc4a3313b7f0435a
Cheng Ren <Crenberry@Xins-MacBook-Air.local>
2014-10-26 18:48:59 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Cheng Ren on 10/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
